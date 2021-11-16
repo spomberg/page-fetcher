5970bee2ba127e646f5456637969f4954ae53406
Marcos Spomberg <marcosspomberg@gmail.com>
2021-11-16 22:53:51 +0000

    Implement page downloader function

---
+++ fetcher.js
@@ -0,0 +1,17 @@
+const url = process.argv[2];
+const filePath = process.argv[3];
+const request = require('request');
+const fs = require('fs');
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+
+  console.log(body);
+  fs.writeFile(filePath, body, (err) => {
+    if (err) throw err;
+    console.log(`Downloaded and saved ${body.length} bytes to ${filePath}`);
+  })
+;});
